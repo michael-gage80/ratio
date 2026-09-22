9df5842463f7ae8f266f04cc282c0d7e27b13ebb
Mike Gage <michael.gage80@icloud.com>
2026-09-22 21:01:21 +0100

    Initial Commit

---
+++ ratio/ratioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ratioApp.swift
+//  ratio
+//
+//  Created by Mike on 22/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct ratioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
